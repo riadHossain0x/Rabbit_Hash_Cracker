e510c6a10f86d5d6868e26cdb4af09e742c28ece
RiyadPathan <bdcyb3rguru@gmail.com>
2018-03-05 22:53:50 -0800

    Add files via upload

---
+++ Hash Crackers/Module1.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Net
+Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Console.Title = "Rabbit Hash Cracker"
+        Console.ForegroundColor = ConsoleColor.DarkGray
+        Console.WriteLine("             __ ")
+        Console.WriteLine("           /    \ ")
+        Console.WriteLine("           | /| \ ")
+        Console.WriteLine("           | ||_/ ")
+        Console.WriteLine("           | || ")
+        Console.WriteLine("     /\    | || ")
+        Console.WriteLine("   /   |   | || ")
+        Console.WriteLine(" /  |\ |   | || ")
+        Console.WriteLine("|__/|| |   | || ")
+        Console.WriteLine("    || |   | || ")
+        Console.WriteLine("    || |   | || ")
+        Console.WriteLine("   /    ~~~    \ ")
+        Console.WriteLine("  |   ^     ^   | ")
+        Console.WriteLine("  |  (o)   (o)  | ")
+        Console.WriteLine("  \      !      / ")
+        Console.WriteLine("  >\     ^     /< ")
+        Console.WriteLine("    \_________/ ")
+        Console.WriteLine("      (\   /) ")
+        Console.WriteLine("      ( >o< )")
+        Console.WriteLine("      (/   \)v1.0 ")
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.DarkGreen
+        Console.WriteLine("# Hello, welcome to Rabbit Hash Cracker based on hashtoolkit.")
+        Console.WriteLine("# Coded By Riyad Pathan (fb/Syn7axErrOr)")
+        Console.WriteLine()
+start:
+        Console.ForegroundColor = ConsoleColor.DarkYellow
+        Console.WriteLine("Enter the Hash (ex: c11855a9627cee943e2637d047f782c9173df781)")
+        Console.ForegroundColor = ConsoleColor.DarkGray
+        Dim hash As String
+        Console.WriteLine()
+        hash = Console.ReadLine()
+        Try
+            Dim req As HttpWebRequest = DirectCast(WebRequest.Create("http://www.hashtoolkit.com/reverse-hash?hash=" & hash), HttpWebRequest)
+            req.Method = "Get"
+            req.UserAgent = "mozilla/4.0 (compatible; MSIE 6.0; windows NT 6.1)"
+            req.ContentType = "Application / x - www-form-urlencoded"
+            Dim Resp As HttpWebResponse
+            Resp = DirectCast(req.GetResponse(), HttpWebResponse)
+            Dim Reqreader As New StreamReader(Resp.GetResponseStream())
+            Dim Result As String = Reqreader.ReadToEnd
+            If Result.Contains("Invalid Input!") Then
+                Console.WriteLine("Invalid Input")
+            Else
+                Try
+                    Dim Alg As String = (Split(Result, "<td>")(1).Split("</td>")(0))
+                    Dim Decry As String = (Split(Result, "<span title=""decrypted " & Alg & " hash"">")(1).Split("</td>")(0))
+                    Console.WriteLine()
+                    Console.ForegroundColor = ConsoleColor.DarkRed
+                    Console.WriteLine("Algorithm: " + Alg & vbNewLine & "Hash: " + hash & vbNewLine & "Decrypted: " + Decry)
+                Catch ex As Exception
+                    Console.WriteLine()
+                    Console.ForegroundColor = ConsoleColor.DarkRed
+                    Console.WriteLine("  ___")
+                    Console.WriteLine(" | __|_ _ _ _ ___ _ _  ")
+                    Console.WriteLine(" | _|| '_| '_/ _ \ '_| ")
+                    Console.WriteLine(" |___|_| |_| \___/_| unable to crack hash... :|")
+                End Try 
+            End If
+        Catch ex As Exception
+
+        End Try
+        Console.WriteLine()
+        GoTo start
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Hash Crackers/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Rabbit Hash Cracker")> 
+<Assembly: AssemblyDescription("advance hash cracker program")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Rabbit Hash Cracker")> 
+<Assembly: AssemblyCopyright("Copyright © 2018 Riyad Pathan")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("00a1fff0-fa9c-42ac-912f-5e3450b67764")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
